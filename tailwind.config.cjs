af431f508609e45bca1cf00dd468468e89d02b8d
Rayan-Aguiar <rayansiqueira@gmail.com>
2022-10-31 13:23:37 -0300

    Configurando RadixUi

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,31 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    colors: {
+      gray:{
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+      cyan:{
+        500: '#81d8f7',
+        300: '#9be4fb'
+      }
+    },
+
+    extend: {
+      sans: 'Inter, sans-serif',
+    },
   },
   plugins: [],
 }
